b91ee3d704f7a736112fd4af6dfdef544bd384a3
Soren <klinkwort92@gmail.com>
2021-05-02 21:33:13 +0200

    Creation of array and methods
    Added an array to store the words that needs to be checked
    Added methods to check the words and one to get the words from the user

--- palindrome/Program.cs
+++ palindrome/Program.cs
@@ -7,10 +7,60 @@ namespace palindrome
         static void Main(string[] args)
         {
             // Variables
+            string[] wordsToCheck = new string[5];
 
             // Methods
 
+            bool checkWord(string word)
+            {
+                char[] wordToArray = word.ToCharArray();
+
+                //Reversing the array
+                Array.Reverse(wordToArray);
+
+                string reversedWord = String.Join("", wordToArray).ToLower();
+
+                if (word.ToLower() == reversedWord) {
+
+                    return true;
+
+                }
+
+                return false;
+       
+            }
+
+            void getWordFromUser(int numberInLine, string text)
+            {
+                Console.Write(text);
+
+                wordsToCheck[numberInLine] = Console.ReadLine();
+            }
+
             // Execution
+
+
+            //First we get the words from the user
+            for (int i = 0; i < wordsToCheck.Length; i++)
+            {
+                getWordFromUser(i, $"#{i + 1} word to check: ");
+            }
+
+            //Then we check the words and inform the user
+            for (var i = 0; i < wordsToCheck.Length; i++)
+            {
+
+                if (checkWord(wordsToCheck[i]))
+                {
+                    Console.WriteLine($"{wordsToCheck[i]} is a palindrome");
+
+                } else
+                {
+                    Console.WriteLine($"{wordsToCheck[i]} is not palindrome");
+
+                }
+
+            }
 
         }
     }
